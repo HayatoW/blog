e6b0147a6df5a8c05a2cc4a45d3d94bc3807fd91
Hayato Watanabe <solideye@me.com>
2022-09-15 01:54:32 +0900

    Add home page

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,7 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async throws -> View in
-        return try await req.view.render("hello", ["name": "Leaf"])
+    app.get { req async throws -> View in
+        return try await req.view.render("home")
     }
 }
